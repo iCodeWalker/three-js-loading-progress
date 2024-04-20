34c24b28ec5cd0cfc1c770b0d209fc67eb724348
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-04-20 23:47:55 +0530

    added : putting 2 triangle on screen

--- src/script.js
+++ src/script.js
@@ -29,13 +29,13 @@ const overlayMaterial = new THREE.ShaderMaterial({
   vertexShader: `
     void main()
     {
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        gl_Position = vec4(position, 1.0);
     }
     `,
   fragmentShader: `
     void main()
     {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0)
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
     `,
 });
